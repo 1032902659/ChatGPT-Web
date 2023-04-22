259cb1caaa9d0efff72ff8da5713415ad13b2f77
lishixuan <94160612+1032902659@users.noreply.github.com>
2023-04-22 16:07:00 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>联系我</span>
+    <a href="https://qm.qq.com/cgi-bin/qm/qr?k=BGdGHQfXBbPTifZPjZA8xC8aOBu2Q1Fs&noverify=0" target="_blank" class="text-blue-500">
+      QQ1032902659
     </a>
   </div>
 </template>
